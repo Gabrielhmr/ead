c322c8ed86c4cabe431f531170e4cc5e6562fb48
Gabriel Rodrigues <gabriel.rodrigues.uece@gmail.com>
2016-09-29 00:24:57 -0300

    added horariobean

--- src/br/com/ead/controller/HorarioBean.java
+++ src/br/com/ead/controller/HorarioBean.java
@@ -1,5 +1,37 @@
 package br.com.ead.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.component.UIForm;
+
+import br.com.ead.dao.AlunoDao;
+import br.com.ead.dao.HorarioDao;
+import br.com.ead.model.Aluno;
+import br.com.ead.model.Horario;
+import br.com.ead.util.FacesUtils;
+import br.com.ead.util.Util;
+
+
+@ManagedBean
 public class HorarioBean {
+	private Horario horario = new Horario();
+	private List<Horario> horarios = new ArrayList<Horario>();
+	
+	private UIForm form;
+	
+	@ManagedProperty("#{facesUtils}")
+	private FacesUtils facesUtils;
+	@ManagedProperty("#{horarioDao}")
+	private HorarioDao horarioDao;
+	
+public void lista() {
+		horarios = horarioDao.listAll();
+		horario = new Horario();
+	}
+
+
 
 }
